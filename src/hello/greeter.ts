39a55861ca4d651fbc0d497f397e2c869e8c1db6
李云飞 <liyunfei02@daojia-inc.com>
2021-01-26 15:39:47 +0800

    :init

---
+++ src/hello/greeter.ts
@@ -0,0 +1,33 @@
+// function greeter (person:string) {
+//   return "Hello," + person;
+// }
+// let user = '11';
+// document.body.innerHTML = greeter(user)
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+// function greeter (person: Person) {
+//   return 'hello' + person.firstName + person.lastName;
+// }
+// let user = {
+//   firstName: 'li',
+//   lastName: 'yf'
+// }
+// document.write(greeter(user))
+// class Student {
+//   fullName: string;
+//   constructor(public firstName: any, public lastName: any) {
+//     this.fullName = firstName + lastName;
+//   }
+// }
+// 数组
+// let list: number[] = [1, 1];
+// let list: Array<number> = [1, 2]
+// 元组
+// let x: [string, number];
+// x = ['hello', 1]
+// 枚举
+enum Color {Red, Green, Blue};
+let c = Color[2];
+console.log(c)
